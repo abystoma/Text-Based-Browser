b8e7af1f17658c6a7832aaab58ae3f50864779f1
b1rkenspanner <zayed.humayun@g.bracu.ac.bd>
2020-10-22 16:57:34 +0600

    init commit

---
+++ Text-Based Browser/task/browser/browser.py
@@ -0,0 +1,39 @@
+from sys import argv
+from os import mkdir
+from collections import deque
+import requests
+
+dir_for_tabs = argv[1]
+
+try:
+    mkdir(dir_for_tabs)
+except FileExistsError:
+    print(f"Directory {dir_for_tabs} already exists")
+
+history = deque()
+
+def save_site_content(data, path):
+    history.append(path)
+    with open(f"./{dir_for_tabs}/{path+'.html'}", 'w',encoding = 'UTF-8') as f:
+        f.write(data)
+
+def go_site(site):
+    address = site if site.startswith("https://") else f"https://{site}"
+    site_content = requests.get(address).text
+    save_site_content(site_content, site)
+    print(site_content)
+
+def go_back():
+
+    with open(f"./{dir_for_tabs}/{history.leftpop()+'.html'}", 'r',encoding = 'UTF-8') as f:
+        print(f.read())
+
+while True:
+    command = input()
+
+    if command == 'exit':
+        break
+    elif command =="back":
+        go_back()
+    elif '.' in command:
+        go_site(command)
