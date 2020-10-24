3cd611618a97c3c66f564b2836f714868442b513
b1rkenspanner <zayed.humayun@g.bracu.ac.bd>
2020-10-24 11:47:54 +0600

    Added HTML parsing

--- Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/task/browser/browser.py
@@ -2,10 +2,9 @@ from sys import argv
 from os import mkdir
 from collections import deque
 import requests
-
+from bs4 import BeautifulSoup
+import colorama
 dir_for_tabs = argv[1]
-
-print("Hello world")
 
 try:
     mkdir(dir_for_tabs)
@@ -14,28 +13,51 @@ except FileExistsError:
 
 history = deque()
 
+def parse_html(html_file):
+
+    soup = BeautifulSoup(html_file, 'html.parser')
+    webpage_text = []
+    tags = soup.find_all(['p', 'title', 'a', 'ul', 'ol', 'il'])
+    for tag in tags:
+        if tag.name == 'a':
+            webpage_text.append(colorama.Fore.BLUE + tag.text + '\n')
+        else:
+            webpage_text.append(colorama.Fore.WHITE + tag.text + '\n')
+
+    return " ".join(webpage_text)
+
+
+def print_webpage(webpage):
+    colorama.init()
+    print(webpage)
+
+
 def save_site_content(data, path):
     history.append(path)
-    with open(f"./{dir_for_tabs}/{path+'.html'}", 'w',encoding = 'UTF-8') as f:
+    with open(f"./{dir_for_tabs}/{path + '.html'}", 'w', encoding='UTF-8') as f:
+        colorama.init()
         f.write(data)
+
 
 def go_site(site):
     address = site if site.startswith("https://") else f"https://{site}"
     site_content = requests.get(address).text
-    save_site_content(site_content, site)
-    print(site_content)
+    parse = parse_html(site_content)
+    save_site_content(parse, site)
+    print_webpage(parse)
+
 
 def go_back():
-
-    with open(f"./{dir_for_tabs}/{history.leftpop()+'.html'}", 'r',encoding = 'UTF-8') as f:
+    with open(f"./{dir_for_tabs}/{history.leftpop() + '.html'}", 'r', encoding='UTF-8') as f:
         print(f.read())
+
 
 while True:
     command = input()
 
     if command == 'exit':
         break
-    elif command =="back":
+    elif command == "back":
         go_back()
     elif '.' in command:
         go_site(command)
